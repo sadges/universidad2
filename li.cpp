0695422a6beb61fe58f5bb3d07375cb2b89fabbd
sadges <143287554+sadges@users.noreply.github.com>
2023-08-27 23:04:45 -0500

    Add files via upload

---
+++ li.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a[]={23,6,84,39};
+	int *b;
+	
+	b = a;
+	
+	for(int c=0;c<5;c++){
+		cout<<"posicion: ["<<c<<"]: "<<*b++<<"  ";
+	}
+}
+
+	
+	
+
+
+
